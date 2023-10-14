0b9895e741d82639d2d8936c52804d925211c4f9
Hemaet33 <hemaet3333@gmail.com>
2023-10-14 11:29:06 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/all.min.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <title>Document</title>
+</head>
+<body>
+  <div class="header">
+    <div class="social">
+      <span><a href=""><i class="fa-brands fa-facebook-f" style="color: #ffffff;"></i></a></span>
+    </div>
+    <nav>
+      <div class="left">
+        <a href="index.php" class="logo">Blog</a>
+      </div>
+      <ul class="right">
+        <li><a href="">Login</a></li>
+        <li><a href="">Register</a></li>
+        <li><a href="">Logout</a></li>
+      </ul>
+    </nav>
+  </div>
+
+  <script src="./js/all.min.js"></script>
+</body>
+</html>
