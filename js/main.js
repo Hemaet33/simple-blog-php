aad3bb86ced05eb9163f41231a94d898b010721d
Hemaet33 <hemaet3333@gmail.com>
2023-10-22 01:40:58 +0600

    changes

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 const profile = document.getElementById("profile");
 
-document.querySelector('.header nav .profile-avatar').addEventListener("click",()=>{
+document.querySelector('.header nav .profile-avatar')?.addEventListener("click",()=>{
   if(profile.getAttribute("data")=="visible"){
     profile.style.right="-100%";
+    profile.style.display="none";
     profile.setAttribute("data",'invisible')
   }else{
+    profile.style.display="flex";
     profile.style.right="0px";
     profile.setAttribute("data",'visible')
   }
 });
-document.querySelector('.right-hamburger .profile-avatar').addEventListener("click",()=>{
+document.querySelector('.right-hamburger .profile-avatar')?.addEventListener("click",()=>{
   if(profile.getAttribute("data")=="visible"){
     profile.style.right="-100%";
+    profile.style.display="none";
     profile.setAttribute("data",'invisible')
   }else{
+    profile.style.display="flex";
     profile.style.right="0px";
     profile.setAttribute("data",'visible')
   }
@@ -21,14 +25,17 @@ document.querySelector('.right-hamburger .profile-avatar').addEventListener("cli
 
 document.querySelector('#profile .cancel').addEventListener("click",()=>{
     profile.style.right="-100%";
+    profile.style.display="none";
     profile.setAttribute("data",'invisible')
 })
 
 
 const humburger = document.querySelector('.right-hamburger');
 document.querySelector('.hum-action').addEventListener('click',()=>{
+  humburger.style.display="block";
   humburger.style.right="0px";
 });
 document.querySelector('.right-hamburger span.cancel').addEventListener('click',()=>{
   humburger.style.right="-100%";
+  humburger.style.display="none";
 });
